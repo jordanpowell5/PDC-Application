4f135bba847e5dc0556b1da401dc988a3d289a1e
Jordan Powell <97560409+jordanpowell5@users.noreply.github.com>
2022-04-19 17:20:44 -0500

    Add files via upload
    Basic Functionality Added!

--- PDC Appointment Scheduler.py
+++ PDC Appointment Scheduler.py
@@ -6,6 +6,8 @@ Installation Instructions: TBD
 '''
 
 from tkinter import *
+from random import *
+
 class Appointment():
     def __init__(self, firstname, lastname, major, year, groupsize, appointmentType, date, time, room, coach):
         #__init__ fucntion used to define all of the characteristics of an appointment
@@ -36,7 +38,7 @@ class Coach():
         Coach.email = email
         Coach.availability = "Open"
 
-class scheduler():
+class scheduler(Room):
     def __init__(self):
         #The__init__function was inspired by the Loan Calculator example in class
         window = Tk()
@@ -83,52 +85,120 @@ class scheduler():
         Button(window, text = "Schedule", command = self.get_appointment).grid(row = 13, column = 2, sticky = E)
         Button(window, text = "Help").grid(row = 14, column = 2, sticky = E)
 
+
         window.mainloop()
         
     def get_appointment(self):
     #This function creates the appointment, calling schedule room and schedule coach, to display the details for the appointment on the interface
-        Appointment(self.firstname,self.lastname,self.major,self.year,self.groupsize,self.appointmentType,self.date,self.time,self.get_room_assignment,'''self.get_coach_assignment''')
-        self.schedule_room()
-        #self.schedule_coach()
+        Appointment(self.firstname,self.lastname,self.major,self.year,self.groupsize,self.appointmentType,self.date,self.time,'TBD','TBD')
+        self.schedule_room(rooms)
+        self.schedule_coach(coaches)
 
-    def get_room_assignment(self):
+    def get_room_assignment(self,roomlist,takenlist):
     #This function chooses a room to assign to the appointment picking between a list containing all of the PDC's available rooms
-        room1 = Room('2212E')
-        room2 = Room('2212H')
-        room3 = Room('2212J')
-        room4 = Room('2212K')
-        rooms = [room1, room2, room3, room4]
-        count = len(rooms)-1
+        count = len(roomlist)-1
         while count >= 0:
-            if (rooms[count].availability == 'open'):
-                rooms[count].assign_room()
-                return rooms[count].name
-            count -= 1
-            print('help')
-                
-    def schedule_room(self):
-    #This function displays the room assignment
-        self.room.set(self.get_room_assignment())
+            #if (roomlist[count].availability == 'open'):
+            if(roomlist[count] not in takenlist):
+                #roomlist[count].assign_room()
+                takenRooms.append(roomlist[count])
+                #return roomlist[count].name
+                return roomlist[count]
+            count -=1
+        return 'Rooms are Full'
+            
 
-    '''
-    def get_coach_assignment(self):
+    def schedule_room(self, roomlist):
+    #This function displays the room assignment
+        self.room.set(self.get_room_assignment(roomlist,takenRooms))
+        '''
+        if(self.time == '8 am'):
+            self.room.set(self.get_room_assignment(roomlist,takenRooms8am))
+        elif(self.time == '9 am'):
+            self.room.set(self.get_room_assignment(roomlist,takenRooms9am))
+        elif(self.time == '10 am'):
+            self.room.set(self.get_room_assignment(roomlist,takenRooms10am))
+        elif(self.time == '11 am'):
+            self.room.set(self.get_room_assignment(roomlist,takenRooms11am))
+        elif(self.time == '12 pm'):
+            self.room.set(self.get_room_assignment(roomlist,takenRooms12pm))
+        elif(self.time == '1 pm'):
+            self.room.set(self.get_room_assignment(roomlist,takenRooms1pm))
+        elif(self.time == '2 pm'):
+            self.room.set(self.get_room_assignment(roomlist,takenRooms2pm))
+        elif(self.time == '3 pm'):
+            self.room.set(self.get_room_assignment(roomlist,takenRooms3pm))
+        elif(self.time == '4 pm'):
+            self.room.set(self.get_room_assignment(roomlist,takenRooms4pm))
+        '''
+        
+    def get_coach_assignment(self,coachlist, ctakenlist):
         #This function chooses a coach to assign to a given appointment from the list coaches based upon alike majors
-        coach1 = Coach('Jordan Powell', 'SCM', 'Junior', 'jordan.powell@tcu.edu')
-        coach2 = Coach('Abraham Salinas', 'Marketing', 'Senior', 'A.SALINAS@tcu.edu')
-        coach3 = Coach('Bridget French','Finance','Senior','BRIDGET.FRENCH@tcu.edu')
-        coach4 = Coach('Biffy Totsi','BIS','Junior','ELIZABETH.TOSTI@tcu.edu')
-        coaches = [coach1, coach2, coach3, coach4]
-        count2 = len(coaches)-1
+        count2 = len(coachlist)-1
         while (count2>=0):
-            #Currently an infinte loop is occuring
-            if(coaches[count2].major == self.major):
-                return coaches[count2].name
-    '''
-    
-    #def schedule_coach(self):
+            if(coachlist[count2] not in ctakenlist):
+                ctakenlist.append(coachlist[count2])
+                return coachlist[count2]
+            count2 -=1
+        return 'No coaches avilable'
+
+    def schedule_coach(self, coachlist):
         #This function displays the coach assignment
-        #self.coach.set(self.get_coach_assignment())
-    
+        self.coach.set(self.get_coach_assignment(coachlist,takenCoaches))
+        '''
+        if(self.time == '8 am'):
+            self.coach.set(self.get_coach_assignment(coachlist,takenCoaches8am))
+        elif(self.time == '9 am'):
+            self.coach.set(self.get_coach_assignment(coachlist,takenCoaches9am))
+        elif(self.time == '10 am'):
+            self.coach.set(self.get_coach_assignment(coachlist,takenCoaches10am))
+        elif(self.time == '11 am'):
+            self.coach.set(self.get_coach_assignment(coachlist,takenCoaches11am))
+        elif(self.time == '12 pm'):
+            self.coach.set(self.get_coach_assignment(coachlist,takenCoaches12pm))
+        elif(self.time == '1 pm'):
+            self.coach.set(self.get_coach_assignment(coachlist,takenCoaches1pm))
+        elif(self.time == '2 pm'):
+            self.coach.set(self.get_coach_assignment(coachlist,takenCoaches2pm))
+        elif(self.time == '3 pm'):
+            self.coach.set(self.get_coach_assignment(coachlist,takenCoaches3pm))
+        elif(self.time == '4 pm'):
+            self.coach.set(self.get_coach_assignment(coachlist,takenCoaches4pm))
+        #else:
+            #self.coach.set('Not a valid time input')
+        '''
+
+#room1 = Room('2212E')
+#room2 = Room('2212H')
+#room3 = Room('2212J')
+#room4 = Room('2212K')
+#rooms = [room1, room2, room3, room4]
+rooms = ['2212E', '2212H', '2212J', '2212K']  
+#coach1 = Coach('Jordan Powell', 'SCM', 'Junior', 'jordan.powell@tcu.edu')
+#coach2 = Coach('Abraham Salinas', 'Marketing', 'Senior', 'A.SALINAS@tcu.edu')
+#coach3 = Coach('Bridget French','Finance','Senior','BRIDGET.FRENCH@tcu.edu')
+#coach4 = Coach('Biffy Totsi','BIS','Junior','ELIZABETH.TOSTI@tcu.edu')
+coaches = ['Jordan Powell', 'Abraham Salinas', 'Bridget French', 'Biffy Totsi']
+takenRooms = []
+takenRooms8am = []
+takenRooms9am = []
+takenRooms10am = []
+takenRooms11am = []
+takenRooms12pm = []
+takenRooms1pm = []
+takenRooms2pm = []
+takenRooms3pm = []
+takenRooms4pm = []
+takenCoaches = []
+takenCoaches8am = []
+takenCoaches9am = []
+takenCoaches10am = []
+takenCoaches11am = []
+takenCoaches12pm = []
+takenCoaches1pm = []
+takenCoaches2pm = []
+takenCoaches3pm = []
+takenCoaches4pm = []
 scheduler()  # Create GUI
 
 
