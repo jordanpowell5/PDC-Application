2c6ee2d75d6e1fb7da3be07ff141c9004c3461ea
Jordan Powell <97560409+jordanpowell5@users.noreply.github.com>
2022-04-20 21:03:43 -0500

    Add files via upload
    Data Validation and Advanced Functionality with the ability to schedule coaches and rooms by hour!

--- PDC Appointment Scheduler.py
+++ PDC Appointment Scheduler.py
@@ -6,39 +6,10 @@ Installation Instructions: TBD
 '''
 
 from tkinter import *
-from random import *
 
-class Appointment():
-    def __init__(self, firstname, lastname, major, year, groupsize, appointmentType, date, time, room, coach):
-        #__init__ fucntion used to define all of the characteristics of an appointment
-        Appointment.firstname = firstname
-        Appointment.lastname = lastname
-        Appointment.major = major
-        Appointment.year = year
-        Appointment.groupsize = groupsize
-        Appointment.appointmentType = appointmentType
-        Appointment.date = date
-        Appointment.time = time
-        Appointment.room = room
-        Appointment.coach = coach
-class Room():
-    def __init__(self, name):
-        #__init__ fucntion used to define all of the characteristics of a room
-        Room.name = name
-        Room.availability = 'open'
-    def assign_room(self):
-        #Once the room has been assigned to an appointment this function is used to change the avilability of the room
-        self.availability = 'taken'
-class Coach():
-    def __init__(self, name, major, year, email):
-        #__init__ fucntion used to define all of the characteristics of an coach
-        Coach.name = name
-        Coach.major = major
-        Coach.year = year
-        Coach.email = email
-        Coach.availability = "Open"
-
-class scheduler(Room):
+#Thank you to https://stackoverflow.com/questions/52645557/type-conversion-in-python-stringvar-to-string for the .get() method
+    
+class scheduler():
     def __init__(self):
         #The__init__function was inspired by the Loan Calculator example in class
         window = Tk()
@@ -90,7 +61,8 @@ class scheduler(Room):
         
     def get_appointment(self):
     #This function creates the appointment, calling schedule room and schedule coach, to display the details for the appointment on the interface
-        Appointment(self.firstname,self.lastname,self.major,self.year,self.groupsize,self.appointmentType,self.date,self.time,'TBD','TBD')
+        while (self.appointmentType.get() not in appointmentTypes):
+            self.appointmentTypeErrorMessage()
         self.schedule_room(rooms)
         self.schedule_coach(coaches)
 
@@ -98,11 +70,8 @@ class scheduler(Room):
     #This function chooses a room to assign to the appointment picking between a list containing all of the PDC's available rooms
         count = len(roomlist)-1
         while count >= 0:
-            #if (roomlist[count].availability == 'open'):
             if(roomlist[count] not in takenlist):
-                #roomlist[count].assign_room()
-                takenRooms.append(roomlist[count])
-                #return roomlist[count].name
+                takenlist.append(roomlist[count])
                 return roomlist[count]
             count -=1
         return 'Rooms are Full'
@@ -110,27 +79,25 @@ class scheduler(Room):
 
     def schedule_room(self, roomlist):
     #This function displays the room assignment
-        self.room.set(self.get_room_assignment(roomlist,takenRooms))
-        '''
-        if(self.time == '8 am'):
+        if(self.time.get() == '8 am'):
             self.room.set(self.get_room_assignment(roomlist,takenRooms8am))
-        elif(self.time == '9 am'):
+        elif(self.time.get() == '9 am'):
             self.room.set(self.get_room_assignment(roomlist,takenRooms9am))
-        elif(self.time == '10 am'):
+        elif(self.time.get() == '10 am'):
             self.room.set(self.get_room_assignment(roomlist,takenRooms10am))
-        elif(self.time == '11 am'):
+        elif(self.time.get() == '11 am'):
             self.room.set(self.get_room_assignment(roomlist,takenRooms11am))
-        elif(self.time == '12 pm'):
+        elif(self.time.get() == '12 pm'):
             self.room.set(self.get_room_assignment(roomlist,takenRooms12pm))
-        elif(self.time == '1 pm'):
+        elif(self.time.get() == '1 pm'):
             self.room.set(self.get_room_assignment(roomlist,takenRooms1pm))
-        elif(self.time == '2 pm'):
+        elif(self.time.get() == '2 pm'):
             self.room.set(self.get_room_assignment(roomlist,takenRooms2pm))
-        elif(self.time == '3 pm'):
+        elif(self.time.get() == '3 pm'):
             self.room.set(self.get_room_assignment(roomlist,takenRooms3pm))
-        elif(self.time == '4 pm'):
+        elif(self.time.get() == '4 pm'):
             self.room.set(self.get_room_assignment(roomlist,takenRooms4pm))
-        '''
+
         
     def get_coach_assignment(self,coachlist, ctakenlist):
         #This function chooses a coach to assign to a given appointment from the list coaches based upon alike majors
@@ -144,41 +111,40 @@ class scheduler(Room):
 
     def schedule_coach(self, coachlist):
         #This function displays the coach assignment
-        self.coach.set(self.get_coach_assignment(coachlist,takenCoaches))
-        '''
-        if(self.time == '8 am'):
+        if(self.time.get() == '8 am'):
             self.coach.set(self.get_coach_assignment(coachlist,takenCoaches8am))
-        elif(self.time == '9 am'):
+        elif(self.time.get() == '9 am'):
             self.coach.set(self.get_coach_assignment(coachlist,takenCoaches9am))
-        elif(self.time == '10 am'):
+        elif(self.time.get() == '10 am'):
             self.coach.set(self.get_coach_assignment(coachlist,takenCoaches10am))
-        elif(self.time == '11 am'):
+        elif(self.time.get() == '11 am'):
             self.coach.set(self.get_coach_assignment(coachlist,takenCoaches11am))
-        elif(self.time == '12 pm'):
+        elif(self.time.get() == '12 pm'):
             self.coach.set(self.get_coach_assignment(coachlist,takenCoaches12pm))
-        elif(self.time == '1 pm'):
+        elif(self.time.get() == '1 pm'):
             self.coach.set(self.get_coach_assignment(coachlist,takenCoaches1pm))
-        elif(self.time == '2 pm'):
+        elif(self.time.get() == '2 pm'):
             self.coach.set(self.get_coach_assignment(coachlist,takenCoaches2pm))
-        elif(self.time == '3 pm'):
+        elif(self.time.get() == '3 pm'):
             self.coach.set(self.get_coach_assignment(coachlist,takenCoaches3pm))
-        elif(self.time == '4 pm'):
+        elif(self.time.get() == '4 pm'):
             self.coach.set(self.get_coach_assignment(coachlist,takenCoaches4pm))
-        #else:
-            #self.coach.set('Not a valid time input')
-        '''
+        else:
+            self.coach.set('Not a valid time input')
 
-#room1 = Room('2212E')
-#room2 = Room('2212H')
-#room3 = Room('2212J')
-#room4 = Room('2212K')
-#rooms = [room1, room2, room3, room4]
+    def appointmentTypeErrorMessage(self):
+        error = Tk()
+        error.title("Invalid Appointment Type")
+        Label(error, text = "Please select a valid appointment type from the following list:").grid(row = 1, column = 1, sticky = W)
+        Label(error, text = "Mock Interview").grid(row = 2, column = 1, sticky = W)
+        Label(error, text = "Presentation Coaching").grid(row = 3, column = 1, sticky = W)
+        Label(error, text = "Resume Review").grid(row = 4, column = 1, sticky = W)
+        Label(error, text = "Room Reservation").grid(row = 5, column = 1, sticky = W)
+        error.mainloop()
+
 rooms = ['2212E', '2212H', '2212J', '2212K']  
-#coach1 = Coach('Jordan Powell', 'SCM', 'Junior', 'jordan.powell@tcu.edu')
-#coach2 = Coach('Abraham Salinas', 'Marketing', 'Senior', 'A.SALINAS@tcu.edu')
-#coach3 = Coach('Bridget French','Finance','Senior','BRIDGET.FRENCH@tcu.edu')
-#coach4 = Coach('Biffy Totsi','BIS','Junior','ELIZABETH.TOSTI@tcu.edu')
 coaches = ['Jordan Powell', 'Abraham Salinas', 'Bridget French', 'Biffy Totsi']
+appointmentTypes =['Mock Interview', 'Presentation Coaching', 'Resume Review', 'Room Reservation']
 takenRooms = []
 takenRooms8am = []
 takenRooms9am = []
